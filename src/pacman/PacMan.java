e8cfe6f9eb72cfc8b05de6ac5c94e5a67c966312
Michelle <114263903+michaeltx130@users.noreply.github.com>
2024-04-17 21:58:08 -0700

    unidad 3: ejercicio 20 corregido
    He corregido el error del programa anterior y he añadido la función de poder mover el elemento Graphic

--- src/pacman/PacMan.java
+++ src/pacman/PacMan.java
@@ -1,3 +1,6 @@
+//Michelle Jesús Obeso Sánchez IDS TV
+//Cambios corregidos en la actividad
+
 package pacman;
 
 import java.awt.EventQueue;
@@ -10,63 +13,80 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import javax.swing.JButton;
 import java.awt.Font;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class PacMan {
 
-	private JFrame frame;
+    private JFrame frame;
+    private int x = 260;
+    private int y = 160;
 
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					PacMan window = new PacMan();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    PacMan window = new PacMan();
+                    window.frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public PacMan() {
+        initialize();
+    }
+
+    private void initialize() {
+        frame = new JFrame();
+        frame.setLocationRelativeTo(null);
+        frame.setTitle("PAC-MAN :v");
+        frame.setResizable(false);
+        frame.setBounds(100, 100, 550, 450);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel panelSoth = new JPanel();
+        panelSoth.setBackground(new Color(0, 0, 255));
+        frame.getContentPane().add(panelSoth, BorderLayout.SOUTH);
+
+        JButton btnNewButton = new JButton("Reiniciar");
+        btnNewButton.setForeground(new Color(0, 0, 0));
+        btnNewButton.setFont(new Font("Cooper Black", Font.PLAIN, 15));
+        panelSoth.add(btnNewButton);
+
+        JPanel panelCenter = new JPanel() {
+            @Override
+            public void paintComponent(Graphics g) {
+                super.paintComponent(g);
+
+                Graphics2D g2d = (Graphics2D) g;
+
+                g.setColor(Color.YELLOW);
+                g2d.fillOval(x, y, 30, 30);
+            }
+        };
+        panelCenter.addKeyListener(new KeyAdapter() {
+        	@Override
+        	public void keyTyped(KeyEvent e) {
+        		if(e.getKeyChar() == 'd' || e.getKeyChar() == 'D') {
+					x+=5;
+					panelCenter.repaint();
+				} else if(e.getKeyChar() == 'w' || e.getKeyChar() == 'W') {
+					y-=5;
+					panelCenter.repaint();
+				} else if(e.getKeyChar() == 'a' || e.getKeyChar() == 'A') {
+					x-=5;
+					panelCenter.repaint();
+				} else if(e.getKeyChar() == 's' || e.getKeyChar() == 'S') {
+					y+=5;
+					panelCenter.repaint();
 				}
-			}
-		});
-	}
-
-	public PacMan() {
-		initialize();
-	}
-
-	private void initialize() {
-		frame = new JFrame();
-		frame.setLocationRelativeTo(null);
-		frame.setTitle("PAC-MAN :v");
-		frame.setResizable(false);
-		frame.setBounds(100, 100, 550, 450);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		JPanel panelSoth = new JPanel();
-		panelSoth.setBackground(new Color(0, 0, 255));
-		frame.getContentPane().add(panelSoth, BorderLayout.SOUTH);
-		
-		JButton btnNewButton = new JButton("Reiniciar");
-		btnNewButton.setForeground(new Color(0, 0, 0));
-		btnNewButton.setFont(new Font("Cooper Black", Font.PLAIN, 15));
-		panelSoth.add(btnNewButton);
-		
-		JPanel panelCenter = new JPanel() {
-
-			@Override
-			public void paintComponent(Graphics g) {
-				
-			super.paintComponent(g);
-			
-			Graphics2D g2d = (Graphics2D) g;
-			
-			//Cielo
-			g2d.setColor(Color.decode("#87CEEB"));
-			g2d.fillRect(0, 0, 700, 650);
-			
-			
-			panelCenter.setBackground(new Color(0, 0, 0));
-			frame.getContentPane().add(panelCenter, BorderLayout.CENTER);
-		}
-		};
-	}
+        	}
+        });
+        panelCenter.setBackground(new Color(0, 0, 0));
+        panelCenter.setFocusable(true); 
+        frame.getContentPane().add(panelCenter, BorderLayout.CENTER);
+    }
 }
-	
