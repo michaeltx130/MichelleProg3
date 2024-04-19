4154620a9bc98ebb0f70025a00a538be7569612a
Michelle <114263903+michaeltx130@users.noreply.github.com>
2024-04-19 09:36:58 -0700

    unidad 3: ejercicio 21

--- src/pacman/PacMan.java
+++ src/pacman/PacMan.java
@@ -6,6 +6,7 @@ package pacman;
 import java.awt.EventQueue;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -15,12 +16,15 @@ import javax.swing.JButton;
 import java.awt.Font;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class PacMan {
+public class PacMan implements KeyListener{
 
     private JFrame frame;
-    private int x = 260;
-    private int y = 160;
+    private JPanel panelCenter = new JPanel();
+
+    Player jugador = new Player(260, 160, 30, 30, " ");
+    Obstaculo obstaculo = new Obstaculo(400, 60, 20, 250, " ");
 
     public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
@@ -50,13 +54,13 @@ public class PacMan {
         JPanel panelSoth = new JPanel();
         panelSoth.setBackground(new Color(0, 0, 255));
         frame.getContentPane().add(panelSoth, BorderLayout.SOUTH);
-
+        
         JButton btnNewButton = new JButton("Reiniciar");
         btnNewButton.setForeground(new Color(0, 0, 0));
         btnNewButton.setFont(new Font("Cooper Black", Font.PLAIN, 15));
         panelSoth.add(btnNewButton);
 
-        JPanel panelCenter = new JPanel() {
+        panelCenter = new JPanel() {
             @Override
             public void paintComponent(Graphics g) {
                 super.paintComponent(g);
@@ -64,29 +68,58 @@ public class PacMan {
                 Graphics2D g2d = (Graphics2D) g;
 
                 g.setColor(Color.YELLOW);
-                g2d.fillOval(x, y, 30, 30);
+                g2d.fillOval(jugador.x, jugador.y, jugador.w, jugador.h);
+                
+                g.setColor(Color.WHITE);
+                g2d.fillRect(obstaculo.x, obstaculo.y, obstaculo.w, obstaculo.h);
             }
         };
         panelCenter.addKeyListener(new KeyAdapter() {
+        	   	
         	@Override
         	public void keyTyped(KeyEvent e) {
-        		if(e.getKeyChar() == 'd' || e.getKeyChar() == 'D') {
-					x+=5;
-					panelCenter.repaint();
-				} else if(e.getKeyChar() == 'w' || e.getKeyChar() == 'W') {
-					y-=5;
-					panelCenter.repaint();
-				} else if(e.getKeyChar() == 'a' || e.getKeyChar() == 'A') {
-					x-=5;
-					panelCenter.repaint();
-				} else if(e.getKeyChar() == 's' || e.getKeyChar() == 'S') {
-					y+=5;
-					panelCenter.repaint();
-				}
+        		  	    
+        	    if (e.getKeyChar() == 'd' || e.getKeyChar() == 'D') {
+        	        Rectangle posicionJugador = new Rectangle(jugador.x + 5, jugador.y, jugador.w, jugador.h);
+        	        //Verifica la colisión entre el jugador y el obstáculo
+        	        if (!posicionJugador.intersects(obstaculo.getBounds())) {
+        	            jugador.x += 5;
+        	        }
+        	    } else if (e.getKeyChar() == 'w' || e.getKeyChar() == 'W') {
+        	        Rectangle posicionJugador = new Rectangle(jugador.x, jugador.y - 5, jugador.w, jugador.h);
+        	        if (!posicionJugador.intersects(obstaculo.getBounds())) {
+        	            jugador.y -= 5;
+        	        }
+        	    } else if (e.getKeyChar() == 'a' || e.getKeyChar() == 'A') {
+        	        Rectangle posicionJugador = new Rectangle(jugador.x - 5, jugador.y, jugador.w, jugador.h);
+        	        if (!posicionJugador.intersects(obstaculo.getBounds())) {
+        	            jugador.x -= 5;
+        	        }
+        	    } else if (e.getKeyChar() == 's' || e.getKeyChar() == 'S') {
+        	        Rectangle posicionJugador = new Rectangle(jugador.x, jugador.y + 5, jugador.w, jugador.h);
+        	        if (!posicionJugador.intersects(obstaculo.getBounds())) {
+        	            jugador.y += 5;
+        	        }
+        	    }
+
+        	    panelCenter.update(panelCenter.getGraphics());
         	}
         });
         panelCenter.setBackground(new Color(0, 0, 0));
         panelCenter.setFocusable(true); 
         frame.getContentPane().add(panelCenter, BorderLayout.CENTER);
     }
+    
+
+	@Override
+	public void keyTyped(KeyEvent e) {	
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {	
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {	
+	}
 }
---
+++ src/pacman/Player.java
@@ -0,0 +1,68 @@
+package pacman;
+
+import java.awt.Rectangle;
+
+public class Player {
+	
+	int x;
+	int y;
+	int w;
+	int h;
+	String color;
+	
+	public Player() {
+	}
+	
+	public Player(int x, int y, int w, int h, String color) {
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+		this.color = color;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getW() {
+		return w;
+	}
+
+	public void setW(int w) {
+		this.w = w;
+	}
+
+	public int getH() {
+		return h;
+	}
+
+	public void setH(int h) {
+		this.h = h;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	////Esto crea un objeto Rectangle que representa y obtiene los límites del jugador
+	public Rectangle getBounds() {
+        return new Rectangle(x, y, w, h);
+    }
+}
